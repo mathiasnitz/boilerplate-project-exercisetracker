833a32505059b6da823ac87ed674de00d42c9266
matt n <mathiasnitz@gmx.de>
2025-03-14 10:47:44 +0000

    2nd commit

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const cors = require('cors');
 const req = require('express/lib/request');
+require('dotenv').config()
 
+//counter, arrays
 let userIdCounter = 1;
 let users = [];
+let exercises = [];
 
 app.use(cors())
 app.use(express.static('public'))
@@ -16,6 +18,7 @@ app.get('/', (req, res) => {
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//user anlegen
 app.post("/api/users", (req,res) => {
   const userName = req.body.username;
   const newUserId = userIdCounter++;
@@ -30,12 +33,17 @@ app.post("/api/users", (req,res) => {
   res.json({ "name": newUser.name, "_id": newUser._id });
 });
 
+// userspezifische übung anlegen, hinzufügen
 app.post("/api/users/:_id/exercises", (req, res) => {
 
-  const userId = req.body._id;
+  const userId = req.params._id;
   const excDescr = req.body.description;
   const excDur = req.body.duration;
-  const excDate = req.body.date;
+  let excDate = req.body.date;
+
+  if(req.body.date == ""){
+    excDate = new Date();
+  }
 
   for(let i = 0; i < users.length; i++){
 
@@ -57,18 +65,67 @@ app.post("/api/users/:_id/exercises", (req, res) => {
   res.json({ error: "User not found" });
 });
 
+// logs anzeigen
+app.get("/api/users/:_id/logs", (req, res) => {
+
+  const userId = req.params._id;
+  const { from, to, limit } = req.query;
+  
+
+  const user = users.find(user => user._id == userId);
+
+  if (!user) {
+    return res.json({ error: "User not found" });
+  }
+
+  let logs = user.exercises || [];
+
+  //userlogs wahlweise eingegrenzt suchen
+
+  if(from) {
+    const fromDate = new Date(from);
+    logs = logs.filter(entry => new Date(entry.date) >= fromDate);
+  }
+
+  if(to) {
+    const toDate = new Date(to);
+    logs = logs.filter(entry => new Date(entry.date) <= toDate);
+  }
+
+  if(limit) {
+    logs = logs.slice(0, parseInt(limit));
+  }
+
+  //ausgeben der ergebnisse
+
+  res.json({
+    _id: user._id,
+    username: user.name,
+    count: logs.length,
+    log: logs
+  })
+
+});
+
+//user via id suchen
 app.get("/api/users/:_id", (req,res) =>{
 
   const userId = req.params._id;
+  parseInt(userId);
 
   for(let i = 0; i < users.length; i++){
-    if(users[i]._id == userId){
+    if(users[i]._id === userId){
       return res.json(users[i]);
     }
   }
 
   res.status(404).json({ error: "User not found" });
 
+});
+
+// alle user anzeigen
+app.get("/api/users", (req,res) =>{
+  res.json(users);
 });
 
 
